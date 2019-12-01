ffb922cb09bb082e43eb71561910a4c786d6d8f4
AlexSerodio <alequis01@gmail.com>
2019-12-01 17:58:17 -0300

    criado modelo e treino mas com acuracia muito baixa

--- network.py
+++ network.py
@@ -14,28 +14,50 @@ test_path = './dataset/test/'
 
 # load train data
 train_labels = dataset.load_image_labels(train_path)
-train_data = dataset.load_image_data(train_path)
+train_images = dataset.load_image_data(train_path)
 
 # load test data
 test_labels = dataset.load_image_labels(test_path)
-test_data = dataset.load_image_data(test_path)
+test_images = dataset.load_image_data(test_path)
 
 # suffle train data
-combined = list(zip(train_labels, train_data))
+combined = list(zip(train_labels, train_images))
 shuffle(combined)
-train_labels, train_data = zip(*combined)
+train_labels, train_images = zip(*combined)
+train_labels = np.array(train_labels)
+train_images = np.array(train_images)
 
 # suffle test data
-combined = list(zip(test_labels, test_data))
+combined = list(zip(test_labels, test_images))
 shuffle(combined)
-test_labels, test_data = zip(*combined)
+test_labels, test_images = zip(*combined)
+test_labels = np.array(test_labels)
+test_images = np.array(test_images)
 
-print(len(train_labels))
-print(len(train_data))
-print(train_data[0].shape)
+# print(train_images.shape)
+# print(test_images.shape)
 
-print('+------------------+')
+# train_images = train_images / 255.0
+# test_images = test_images / 255.0
 
-print(len(test_labels))
-print(len(test_data))
-print(test_data[0].shape)
+# print(train_images.shape)
+# print(test_images.shape)
+
+# model layers creation
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(dataset.height, dataset.width)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+# model compilation
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# training
+model.fit(train_images, train_labels, epochs=10)
+
+# evaluation
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+print('\nTest accuracy:', test_acc)
--- prepare_dataset.py
+++ prepare_dataset.py
@@ -4,7 +4,10 @@ import numpy as np
 
 file_extension = '.jpg'
 
-def prepare_image_data(path, width, height, output):
+width = 100
+height = 150
+
+def prepare_image_data(path, output):
     file_names = os.listdir(path)
     for file in file_names:
         image = cv2.imread(path + '/' + file, cv2.IMREAD_GRAYSCALE)
@@ -17,11 +20,15 @@ def load_image_labels(path):
     return labels
 
 def load_image_data(path):
+    images = np.array([])
     images = []
     file_names = os.listdir(path)
     for file in file_names:
         image = cv2.imread(path + '/' + file, cv2.IMREAD_GRAYSCALE)
-        images.append(np.asarray(image))
+        # image = cv2.imread(path + '/' + file)
+        # images = np.append(images, image)
+        images.append(image)
+
     return images
 
 def map_label(file_names):
@@ -33,25 +40,25 @@ def map_label(file_names):
 
 def get_name(name):
     if 1060 <= name <= 1122:
-        return 'Chinese horse'
+        return 0 # 'Chinese horse'
     elif 1552 <= name <= 1616:
-        return 'Anhui Barberry'
+        return 1 # 'Anhui Barberry'
     elif 1123 <= name <= 1194:
-        return 'Chinese redbud'
+        return 2 # 'Chinese redbud'
     elif 1195 <= name <= 1267:
-        return 'True indigo'
+        return 3 # 'True indigo'
     elif 2051 <= name <= 2113:
-        return 'Japanese cheesewood'
+        return 4 # 'Japanese cheesewood'
     elif 2166 <= name <= 2230:
-        return 'Camphortree'
+        return 5 # 'Camphortree'
     elif 2347 <= name <= 2423:
-        return 'Deodar'
+        return 6 # 'Deodar'
     elif 2547 <= name <= 2612:
-        return 'Oleander'
+        return 7 # 'Oleander'
     elif 3111 <= name <= 3175:
-        return 'Chinese Toon'
+        return 8 # 'Chinese Toon'
     elif 3447 <= name <= 3510:
-        return 'Canadian popular'
+        return 9 # 'Canadian popular'
 
 
 def format_name(name):
